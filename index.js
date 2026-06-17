326bf4450d444279faaa4e7a1706d992e9c05b57
Banti Kumar <159269614+Banti4750@users.noreply.github.com>
2026-06-17 09:34:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.status(200).send("Hello  World! ")
-})
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🚀 OCI Node.js Server</h1>
+    <p>Everything is working!</p>
+  `);
+});
 
 app.listen(80, "0.0.0.0", () => {
- console.log("Server is running on port 80");
+  console.log("Server running on port 80");
 });
